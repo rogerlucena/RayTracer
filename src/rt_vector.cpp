bb8a05b11d992492f55aa0cfb4a08ae76a5fbcb1
Alexandre Macedo <arj.macedo@gmail.com>
2017-05-06 21:11:54 +0200

    Updated vector

--- src/rt_vector.cpp
+++ src/rt_vector.cpp
@@ -1,1 +1,8 @@
 #include "rt_vector.h"
+
+RtVector::RtVector(double x_, double y_, double z_): x(x_), y(y_), z(z_){}
+
+void print(){
+    std::cout << "[x, y, z] = [" << this->x << ", " << this->y << ", " << this->z << "]";
+}
+
--- src/rt_vector.h
+++ src/rt_vector.h
@@ -3,8 +3,15 @@
 #define RT_VECTOR_H
 
 class RtVector{
+    
+public:
+    RtVector(double x_, double y_, double z_);
+    ~RtVector();
+    void print();
+
+private:
     double x,y,z;
 
-}
+};
 
 #endif /* RT_VECTOR_H */
