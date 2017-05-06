c8ba48499e140302376f807ff51e050a6da8c2d2
Alexandre Macedo <arj.macedo@gmail.com>
2017-05-06 23:26:21 +0200

    Overloaded vector operators

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,10 @@
 
 int main(){
     RtVector dir(1.0, 1.0, 1.0);
+    RtVector dir2(2.0, 2.0, 2.0);
+    dir.print();
+    dir += dir2;
+    dir.print();
   //  dir.print();
     RtRay teste(2.0, 2.0, 2.0, dir); 
     teste.print();
